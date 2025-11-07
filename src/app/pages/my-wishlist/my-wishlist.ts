c939176b00857c17bf98b7d9c9d14e24782722c8
ZidanCorson <zwang@scottlogic.com>
2025-11-07 13:22:54 +0000

    adds wishlist function

--- src/app/pages/my-wishlist/my-wishlist.ts
+++ src/app/pages/my-wishlist/my-wishlist.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { BackButton } from "../../components/back-button/back-button";
 import { EcommerceStore } from '../../ecommerce-store';
+import { ProductCard } from "../../components/product-card/product-card";
 
 @Component({
   selector: 'app-my-wishlist',
-  imports: [BackButton],
+  imports: [BackButton, ProductCard],
   template: `
     <div class="mx-auto max-w-[1200px] py-6 px-4">
       <app-back-button class="mb-6" navigateTo="/products/all">Continue Shopping</app-back-button>  
@@ -13,6 +14,12 @@ import { EcommerceStore } from '../../ecommerce-store';
         <div class="mb-6 flex items-center justify-between">
           <h1 class="text-2xl font-bold">My Wishlist</h1>
           <span class="text-gray-500 text-xl"> {{ store.wishlistCount() }} items </span>
+        </div>
+
+        <div class="responsive-grid">
+          @for (product of store.wishlistItems(); track $index) {
+            <app-product-card [product]="product" />
+          }
         </div>
         
       } @else {  }
