0988ad0c6ba1d2ca7c6437bfb2ff3498d6273726
ZidanCorson <zwang@scottlogic.com>
2025-11-07 13:09:40 +0000

    adds going back button on wishlist page

--- src/app/pages/my-wishlist/my-wishlist.ts
+++ src/app/pages/my-wishlist/my-wishlist.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { BackButton } from "../../components/back-button/back-button";
 
 @Component({
   selector: 'app-my-wishlist',
-  imports: [],
+  imports: [BackButton],
   template: `
-    <p>
-      my-wishlist works!
-    </p>
+    <div class="mx-auto max-w-[1200px] py-6 px-4">
+
+      <app-back-button label="Continue Shopping" navigateTo="/products/all" />
+    </div>
   `,
   styles: ``,
 })
