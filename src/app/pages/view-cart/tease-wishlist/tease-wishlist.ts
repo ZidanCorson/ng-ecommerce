4d80aa9607982f09d582d17e30e43481162e5341
ZidanCorson <zwang@scottlogic.com>
2025-11-10 14:42:07 +0000

    adds tease wishlist bar

--- src/app/pages/view-cart/tease-wishlist/tease-wishlist.ts
+++ src/app/pages/view-cart/tease-wishlist/tease-wishlist.ts
@@ -2,10 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ViewPanel } from "../../../directives/view-panel";
 import { MatIcon } from "@angular/material/icon";
 import { EcommerceStore } from '../../../ecommerce-store';
+import { MatButton} from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { MatActionList } from "@angular/material/list";
 
 @Component({
   selector: 'app-tease-wishlist',
-  imports: [ViewPanel, MatIcon],
+  imports: [ViewPanel, MatIcon, MatButton, RouterLink, MatActionList],
   template: `
     <div appViewPanel class="flex items-center justify-between">
       <div class="flex items-center gap-3">
@@ -15,7 +18,13 @@ import { EcommerceStore } from '../../../ecommerce-store';
           <p class="text-gray-500 text-sm">You have {{store.wishlistCount()}} items saved for later.</p>
         </div>
       </div>
-
+      <div class="flex items-center gap-3">
+        <button matButton routerLink="/wishlist">View All</button>
+        <button matButton="filled" class="flex items-center gap-2">
+         <mat-icon>shopping_cart</mat-icon>
+         Add All to Cart
+        </button>
+      </div>
     </div>
   `,
   styles: ``,
