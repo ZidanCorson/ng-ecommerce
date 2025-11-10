86241acb11fe9b4c80df84dd1ba482d198477d6c
ZidanCorson <zwang@scottlogic.com>
2025-11-10 10:08:17 +0000

    adds list cart items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,9 @@ export const routes: Routes = [
     {
         path: 'wishlist',
         loadComponent: () => import('./pages/my-wishlist/my-wishlist'),
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('./pages/view-cart/view-cart'),
     }
 ];
